297abf9c268ac33697d9547e252869cd33a1c3ac
EgorVoronin06 <voronine15@mail.ru>
2024-11-10 13:39:36 +0300

    Update test script.js

--- test script.js
+++ test script.js
@@ -13,3 +13,30 @@ function addMedication(name, time, medSideEffects) {
     const medList = document.getElementById('medList');
 
 }
+
+
+const { Client } =require('pg');
+const { Client } = require('undici-types');
+// создание клиента с парметром 
+const Client = new Client ({
+    host: 'localhost',
+    user: 'your_username',
+    password: 'your_password',
+    database: 'your_database',
+    port: 5432,
+})
+
+client.connect()
+    .then(() => console.log('connected to PostgreSQL'))
+    .catch (err => console.error('connection error', err.stack));
+
+
+client.query('select now()', (err, res) => {
+    if (err){
+        console.error(err);
+        return;
+
+}
+console.log('Current Time:', res.rows[0]);
+client.end();
+})
